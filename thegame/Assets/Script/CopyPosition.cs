f71225abd90522b786b02a48822f00da274f4bce
pecku <xsajtx@gmail.com>
2021-08-24 17:31:04 +0200

    Destroyable cubes

---
+++ thegame/Assets/Script/CopyPosition.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyPosition : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+    void Update()
+    {
+        GetComponent<Transform>().position = target.position;
+    }
+}
